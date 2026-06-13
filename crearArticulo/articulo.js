1bc1124f77f1db2399169b1387abe217c1959277
eriksawchuk96 <erik.sawchuk@anima.edu.uy>
2026-06-12 23:20:38 -0300

    Subtítulo y CSS

--- crearArticulo/articulo.js
+++ crearArticulo/articulo.js
@@ -6,9 +6,9 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("#autor-web").innerHTML = articulo.autor;
   document.querySelector("#descripcion-tag").innerHTML = articulo.descripcion;
   document.querySelector("#descripcion-tag2").innerHTML = articulo.descripcion2;
-   document.querySelector("#fecha-web").innerHTML = articulo.fecha;
-    document.querySelector("#contenido-web").innerHTML = articulo.contenido;
-    document.querySelector("#imagen-web").src = articulo.imagen;
-    document.querySelector("#imagen-web2").src = articulo.imagen2;
-    document.querySelector("#contenido-web2").innerHTML = articulo.contenido2;
+  document.querySelector("#fecha-web").innerHTML = articulo.fecha;
+  document.querySelector("#contenido-web").innerHTML = articulo.contenido;
+  document.querySelector("#imagen-web").src = articulo.imagen;
+  document.querySelector("#imagen-web2").src = articulo.imagen2;
+  document.querySelector("#contenido-web2").innerHTML = articulo.contenido2;
 });
